f80a5631e419a4243bc1f027c08ed6a5b452c5e8
Nam <58057092+nam-doan16@users.noreply.github.com>
2026-09-10 01:39:36 -0400

    Delete .vitepress directory

--- .vitepress/plugins/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "markdown-it-vuepress-code-snippet-enhanced" {
-  const snippetPlugin: (md: any) => void;
-  export default snippetPlugin;
-}
-
-declare module "*.css" {
-  const content: any;
-  export default content;
-}
